d7a703b17f3f115e1a921b589c9895c0d2ab808e
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2022-12-18 16:35:23 +0530

    added task model

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Tasks } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Tasks[] = [];
 
   getAllTasks() {
     return this.tasks;
